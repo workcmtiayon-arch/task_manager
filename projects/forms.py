a4e1c335a9632682af80cb0adae0c0cee1db8f21
Malik <workcmtiayon@gmail.com>
2026-09-05 23:25:20 +0100

    feat(projects): expose planning fields in project form

--- projects/forms.py
+++ projects/forms.py
@@ -1,11 +1,26 @@
 from django import forms
+
 from .models import Project
 
+
 class ProjectForm(forms.ModelForm):
-    class Meta :
+    class Meta:
         model = Project
-        fields = ['name', 'description']
+        fields = [
+            'name',
+            'description',
+            'planned_start_date',
+            'planned_duration_days',
+            'complexity',
+        ]
         labels = {
-            'name' : 'Project name',
-            'description' : 'Description'
+            'name': 'Project name',
+            'description': 'Description',
+            'planned_start_date': 'Planned start date',
+            'planned_duration_days': 'Planned duration (days)',
+            'complexity': 'Complexity',
+        }
+        widgets = {
+            'planned_start_date': forms.DateInput(attrs={'type': 'date'}),
+            'planned_duration_days': forms.NumberInput(attrs={'min': 1}),
         }
