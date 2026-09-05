76f6e858c09529e6011bf80d71c7d684729196c0
Malik <workcmtiayon@gmail.com>
2026-09-05 23:27:00 +0100

    feat(projects): add completed projects endpoint

--- projects/urls.py
+++ projects/urls.py
@@ -3,6 +3,7 @@ from projects import views
 
 urlpatterns = [
     path('', views.project_list, name='project_list'),
+    path('completed/', views.completed_project_list, name='completed_projects'),
     path('add/', views.add_project, name='project_add'),
     path('update/<int:id>/', views.update_project, name="project_update"),
     path('delete/<int:id>/', views.delete_project, name="project_delete"),
