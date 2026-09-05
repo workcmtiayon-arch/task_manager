c11dceecdd1389e1642c961adda60983dfe2b996
Malik <workcmtiayon@gmail.com>
2026-09-05 23:28:28 +0100

    feat(projects): improve project administration

--- projects/admin.py
+++ projects/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Project
 
-# Register your models here.
-admin.site.register(Project)
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'user',
+        'planned_start_date',
+        'planned_end_date',
+        'complexity',
+        'temporal_status',
+    )
+    list_filter = ('complexity',)
+    search_fields = ('name', 'description', 'user__username', 'user__email')
+    readonly_fields = ('planned_end_date', 'temporal_status')
