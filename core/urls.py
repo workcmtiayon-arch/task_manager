d4f2f83cb3b7c375dada5718812c9c0ecfd79a6b
Malik <workcmtiayon@gmail.com>
2026-09-14 17:44:17 +0100

    Exposer la route de sélection de langue

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,11 @@
-from django.urls import include, path
+from django.urls import path
 from core import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path(
+        'langue/',
+        views.changer_langue,
+        name='changer_langue',
+    ),
 ]
