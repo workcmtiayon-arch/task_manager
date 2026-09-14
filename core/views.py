4d6baf0d90475d78bba3267e25839796cf25f88a
Malik <workcmtiayon@gmail.com>
2026-09-14 17:43:57 +0100

    Ajouter la vue de changement de langue

--- core/views.py
+++ core/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.conf.locale import LANGUAGE_SESSION_KEY
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from django.utils import translation
+from django.utils.http import url_has_allowed_host_and_scheme
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')# test
+
+
+def changer_langue(request):
+    langue = request.POST.get('langue', '')
+    langues_disponibles = {code for code, nom in settings.LANGUAGES}
+
+    if request.method == 'POST' and langue in langues_disponibles:
+        translation.activate(langue)
+        request.session[LANGUAGE_SESSION_KEY] = langue
+        if request.user.is_authenticated and request.user.langue != langue:
+            request.user.langue = langue
+            request.user.save(update_fields=['langue'])
+
+    destination = request.POST.get('suivant') or request.META.get('HTTP_REFERER')
+    if not destination or not url_has_allowed_host_and_scheme(
+        destination, allowed_hosts={request.get_host()}
+    ):
+        destination = reverse('home')
+    return redirect(destination)
